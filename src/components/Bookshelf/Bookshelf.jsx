e2badbaabc8bbd3bf616b01335d85a644ef4c71f
michellejtan <michelle0223@gmail.com>
2025-03-31 05:53:00 -0700

    wip: add evt handlers, create form

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-
-
 const Bookshelf = () => {
 
     const [books, setBooks] = useState([
@@ -12,7 +10,18 @@ const Bookshelf = () => {
         title: '',
         author: '',
     });
+    const handleInputChange = (event) => {
+        setNewBook({ ...newBook, [event.target.name]: event.target.value });
+        console.log([event.target.value]);
+    };
 
+    const handleSubmit = (event) => {
+        event.preventDefault(); //prevent the default behavior of submitting a form
+        // Set our book state with new book at the time of submission
+        setBooks([...books, newBook]);
+        // Reset title and author state to clear our form inputs
+        setNewBook({ title: '', author: '' });
+    };
 
     return (
         <>
@@ -20,6 +29,23 @@ const Bookshelf = () => {
                 <div className="formDiv">
                     <h3>Add a Book</h3>
                     {/* Form will go here */}
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="title">Ttitle: </label>
+                        <input
+                            id="title"
+                            name="title"
+                            value={newBook.title}
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="author">Author: </label>
+                        <input
+                            id="author"
+                            name="author"
+                            value={newBook.author}
+                            onChange={handleInputChange}
+                        />
+                        <button type="submit">Add Book</button>
+                    </form>
                 </div>
 
                 <div className="bookCardsDiv">
